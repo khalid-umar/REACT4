3bd30998583226986a9fd7276df12d985af63dbd
khalid-umar <125467461+khalid-umar@users.noreply.github.com>
2023-10-08 23:34:05 +0530

    Hero.js

---
+++ Hero.js
@@ -0,0 +1,15 @@
+// Hero.js
+import React from 'react';
+
+function Hero() {
+  return (
+    <div className="hero">
+      <h1>Your Name</h1>
+      <p>Email: your.email@example.com</p>
+      <p>Phone: (123) 456-7890</p>
+      <p>Address: 123 Street, City, Country</p>
+    </div>
+  );
+}
+
+export default Hero;
