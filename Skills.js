a16905f1c4c1c243e9dbe5740e1ef357da485df0
khalid-umar <125467461+khalid-umar@users.noreply.github.com>
2023-10-08 23:34:33 +0530

    Skills.js

---
+++ Skills.js
@@ -0,0 +1,18 @@
+// Skills.js
+import React from 'react';
+
+function Skills() {
+  return (
+    <div className="skills">
+      <h2>Technical Skills</h2>
+      <ul>
+        <li>Skill 1</li>
+        <li>Skill 2</li>
+        <li>Skill 3</li>
+        {/* Add more skills as needed */}
+      </ul>
+    </div>
+  );
+}
+
+export default Skills;
