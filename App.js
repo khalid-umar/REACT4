82b58e1cec878473f5a3eced1423e18020a345ef
khalid-umar <125467461+khalid-umar@users.noreply.github.com>
2023-10-08 23:37:36 +0530

    App.js

--- App.js
+++ App.js
@@ -1,5 +1,18 @@
-import "./styles.css";
+// App.js
+import React from 'react';
+import './styles.css';
+import Hero from './components/Hero';
+import Skills from './components/Skills';
+import About from './components/About';
 
-export default function App() {
-  // Code here
+function App() {
+  return (
+    <div className="App">
+      <Hero />
+      <Skills />
+      <About />
+    </div>
+  );
 }
+
+export default App;
