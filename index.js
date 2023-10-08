e8150237d866ca8c0d78f51fb1bc57420eb984dc
khalid-umar <125467461+khalid-umar@users.noreply.github.com>
2023-10-08 23:39:02 +0530

    index.js

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+// index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+import App from './App';
 
-import App from "./App";
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
+ReactDOM.render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
