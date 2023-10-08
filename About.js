3604855e29a556e928546d017eb7b4f56ee3cfa3
khalid-umar <125467461+khalid-umar@users.noreply.github.com>
2023-10-08 23:36:31 +0530

    About.js

--- About.js
+++ About.js
@@ -1,1 +1,18 @@
+// About.js
+import React from 'react';
+
+function About() {
+  return (
+    <div className="about">
+      <h2>About Me</h2>
+      <p>
+        I am passionate about technology and enjoy working on projects related to web development.
+        In my free time, I like to pursue various hobbies and have been involved in several interesting projects.
+        I also have experience in XYZ industry.
+      </p>
+    </div>
+  );
+}
+
+export default About;
 
